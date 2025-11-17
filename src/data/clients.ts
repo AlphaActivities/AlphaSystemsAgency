5f2654d7fae1d1c4c924870a8a4d073494175b42
Josh the Man <gta.alpha.activities@gmail.com>
2025-11-16 19:40:03 -0800

    Updated clients.ts

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -1,7 +1,13 @@
 export const clients = [
   { name: "Aksarben Locksmiths", launchDate: "2025-03-15" },
-  { name: "Tech Solutions Inc", launchDate: "2025-02-20" },
-  { name: "Digital Ventures", launchDate: "2025-01-10" },
+  { name: "Caliber Luxury Rentals", launchDate: "2025-03-01" },
+  { name: "Centre Dentaire", launchDate: "2025-02-28" },
+  { name: "Classy Roofs", launchDate: "2025-02-20" },
+  { name: "Culture Barbershop", launchDate: "2025-02-15" },
+  { name: "Elite Barbershop", launchDate: "2025-02-10" },
+  { name: "Remax", launchDate: "2025-02-05" },
+  { name: "Rocket Baller", launchDate: "2025-01-25" },
+  { name: "Triple W Rentals", launchDate: "2025-01-10" },
 ];
 
 export const getVerifiedLaunchCount = () => clients.length;
