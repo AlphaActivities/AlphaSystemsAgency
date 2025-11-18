1d2e6cf79568b3b56d405c9031840800ae133ab4
Josh the Man <gta.alpha.activities@gmail.com>
2025-11-18 08:36:44 -0800

    Updated Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -119,10 +119,10 @@ export default function Footer() {
                 </a>
               </li>
 
-              <li className="-mt-1 text-white/90 leading-[1.375rem]">
+              <li className="mt-2 text-white/90 leading-[1.375rem]">
                 Consultations by request
               </li>
-              <li className="text-white/90 leading-[1.375rem]">
+              <li className="mt-6 text-white/90 leading-[1.375rem]">
                 Response time under 24 hours
               </li>
             </ul>
