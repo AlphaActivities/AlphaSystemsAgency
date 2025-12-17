b50af9739c8862c59ec2a88270894230ea9b513e
Josh the Man <gta.alpha.activities@gmail.com>
2025-12-16 22:18:22 -0800

    Updated Aksarben.tsx

--- src/pages/work/Aksarben.tsx
+++ src/pages/work/Aksarben.tsx
@@ -33,7 +33,7 @@ export default function Aksarben() {
       const observerOptions = {
         root: null,
         rootMargin: "200px",
-        threshold: 0.2,
+        threshold: 0.05,
       };
 
       const observerCallback = (entries: IntersectionObserverEntry[]) => {
