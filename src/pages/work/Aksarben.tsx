0e447eddf29d1f120e6736407e1ee8ec2853162b
Josh the Man <gta.alpha.activities@gmail.com>
2025-12-26 01:01:07 -0800

    Updated Aksarben.tsx

--- src/pages/work/Aksarben.tsx
+++ src/pages/work/Aksarben.tsx
@@ -396,7 +396,7 @@ export default function Aksarben() {
           Verified Outcome
         </div>
         <div className="relative w-full flex justify-center pt-2 mb-6">
-          <div className="relative inline-flex items-center justify-center min-h-[320px]">
+          <div className="relative inline-flex items-center justify-center min-h-[220px]">
 
             <img
               src="/images/extras/green-ribbon.png"
