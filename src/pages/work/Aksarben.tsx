43a865d542e5cf3ecddc6e223fcebd2940fbb137
Josh the Man <gta.alpha.activities@gmail.com>
2026-01-02 05:19:44 -0800

    Updated Aksarben.tsx

--- src/pages/work/Aksarben.tsx
+++ src/pages/work/Aksarben.tsx
@@ -400,11 +400,8 @@ export default function Aksarben() {
             <img
               src="/images/extras/green-ribbon.png"
               alt="Client Outcome"
-              className="h-[221px] w-auto select-none pointer-events-none relative z-10"
-              width="764"
-              height="221"
-              decoding="async"
-              loading="eager"
+              className="h-12 w-auto select-none pointer-events-none relative z-10"
+              style={{ transform: "scale(4.6)", transformOrigin: "center" }}
             />
           </div>
         </div>
