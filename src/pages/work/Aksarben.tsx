8332ec7e1625cff41fdcac693a78cda41734cd60
Josh the Man <gta.alpha.activities@gmail.com>
2026-01-02 11:36:36 -0800

    Updated Aksarben.tsx

--- src/pages/work/Aksarben.tsx
+++ src/pages/work/Aksarben.tsx
@@ -404,7 +404,7 @@ export default function Aksarben() {
               loading="eager"
               decoding="sync"
               style={{
-                transform: "scale(4.6)",
+                transform: "translateZ(0) scale(4.6)",
                 transformOrigin: "center",
                 willChange: "auto",
                 backfaceVisibility: "hidden"
